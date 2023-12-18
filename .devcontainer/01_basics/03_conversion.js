ebeb8fdbb4f0d719ffce94d4d17bb4b8f863938e
shivam singh <113622685+Shivam7235singh@users.noreply.github.com>
2023-12-18 08:47:02 +0000

    conversion

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -1,7 +1,10 @@
-let score = "33"
+let score = "33abc"
 
 console.log(score);
 
 console.log(typeof score);
 let valueInNumber= Number (score);
 console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
