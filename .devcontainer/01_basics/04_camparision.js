f79425a439ddbd06f6e727964026ac3215da0013
shivam singh <113622685+Shivam7235singh@users.noreply.github.com>
2023-12-18 15:54:08 +0000

    camparision

--- .devcontainer/01_basics/04_camparision.js
+++ .devcontainer/01_basics/04_camparision.js
@@ -7,11 +7,16 @@
 // console.log( "2" > 1);
 // console.log("02" > 1);
 
+//  avoid this type of code
+
 // console.log( null> 0);
 // console.log(null == 0);
 // console.log( null >=0);
 
-console.log( undefined ==0);
-console.log(undefined > 0);
-console.log( undefined< 0);
+// console.log( undefined ==0);
+// console.log(undefined > 0);
+// console.log( undefined< 0);
+
+// === strict check
+console.log("2" === 2);
 
