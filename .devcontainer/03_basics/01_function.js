eab4ee0e6b9880cb19d63668015f25cd01d2a721
shivam singh <113622685+Shivam7235singh@users.noreply.github.com>
2023-12-25 18:24:36 +0000

    function

---
+++ .devcontainer/03_basics/01_function.js
@@ -0,0 +1,41 @@
+// function addTwoNumber(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumber(number1,number2){
+// let result = number1  + number2;
+// console.log("Shivamkumarsingh")
+// return result ;
+return number1+ number2 
+}
+
+addTwoNumber(3 , 4)
+addTwoNumber(3 , "4")
+addTwoNumber()
+addTwoNumber(3 , null)
+
+// const result = addTwoNumber(3 , 4)
+//  console.log("result:  ", result );
+function loginUserMessage(username = "ankur"){
+    if(!username){ 
+    console.log("please inter your user nmae ");
+    return 
+    }
+    return `${username} just logged in `
+}
+console.log(loginUserMessage("shivamsingh"))
+
+// console.log(loginUserMessage())
+
+
+
+
+
+
+
+
+
+
+
+
+
