ab907eafc1a1470020098026e4942e4f2743d378
shivam singh <113622685+Shivam7235singh@users.noreply.github.com>
2023-12-26 14:03:15 +0000

    function

--- .devcontainer/03_basics/01_function.js
+++ .devcontainer/03_basics/01_function.js
@@ -23,19 +23,41 @@ function loginUserMessage(username = "ankur"){
     }
     return `${username} just logged in `
 }
-console.log(loginUserMessage("shivamsingh"))
+// console.log(loginUserMessage("shivamsingh"))
 
 // console.log(loginUserMessage())
 
 
+function  calculatecartprice( val1, val2, ...num1){
+    return num1
 
+}
+//  console.log(calculatecartprice(200,400,678,500,780))
 
+const user = {
+    username : "Shivam singh",
+    price : 1999
+}
+ 
+function handleObject(anyobject){
+    console.log(` username is ${anyobject.username} and price is ${anyobject.price}`);
 
+}
+// handleObject(user)
 
+handleObject({
+    username : "Ankur",
+    price :  1234
+})
 
+const myNewArray = [  200,300,4000]
 
+function returnSecondValue(getArray){
+    return getArray[1]
+}
 
-
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,4000]));
 
 
 
