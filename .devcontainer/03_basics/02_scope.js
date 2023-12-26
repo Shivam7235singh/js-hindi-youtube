79ab1a1b533bdd69a62d727b36c6465894942ca4
shivam singh <113622685+Shivam7235singh@users.noreply.github.com>
2023-12-26 14:54:31 +0000

    scope

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,62 @@
+var c = 23
+let a= 300
+
+if(true){
+    let a = 25 ,
+    // const  b = 29,
+    c = 89
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = " hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)
+
+    two ()
+}
+// one()
+
+if(true){
+    const username = "Shivamkumar"
+    if(username === "Shivamkumar"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ************************************* interesting ************************************
+
+console.log(addone(5));
+
+function addone(num){
+    return num +1
+}
+//   addTwo(5)
+ const addTwo = function(num){
+    return num + 2
+ }
+
+console.log(addTwo(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
